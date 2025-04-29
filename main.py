2bf3c74fe8f9c7c9e667f1c33c393e4d4ec240fe
bupkis01 <ahammedfavas2004@gmail.com>
2025-04-29 17:07:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# main.py
-# Entry point for the bot.
+from threading import Thread
+from server import app
 
-from scheduler import start_bot
+def run_flask():
+    app.run(host="0.0.0.0", port=8080)
 
-if __name__ == "__main__":
-    start_bot()
+Thread(target=run_flask).start()
